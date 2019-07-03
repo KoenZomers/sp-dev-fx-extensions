7ba7e7304e7f6ded75fa4f22f992ae7a95d4cc46
Koen Zomers <koen@zomers.eu>
2019-07-03 17:02:01 +0200

    Initial commit

---
+++ samples/react-application-tenant-global-navbar2/TenantGlobalNavBarProvisionCustomizer.ps1
@@ -0,0 +1,7 @@
+﻿#Connect-PnPOnline "https://<your-tenant>.sharepoint.com/sites/<your-site>" -UseWebLogin
+
+Add-PnPCustomAction -Title "TenantGlobalNavBarCustomAction" `
+					-Name "TenantGlobalNavBarCustomAction" `
+					-Location "ClientSideExtension.ApplicationCustomizer" `
+					-ClientSideComponentId "33ebce34-5b96-4e6e-b470-5eaea3b7e070" `
+					-ClientSideComponentProperties "{""TopMenuTermSet"":""TenantGlobalNavBar"",""BottomMenuTermSet"":""TenantGlobalFooterBar""}"
